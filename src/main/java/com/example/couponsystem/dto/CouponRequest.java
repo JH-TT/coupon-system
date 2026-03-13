7410b01a84b1fff8d4c4a612d982010e732df179
JH-TT <leejongho9803@gmail.com>
2026-03-14 01:54:24 +0900

    feat: request dto 생성
    - CouponRequest

---
+++ src/main/java/com/example/couponsystem/dto/CouponRequest.java
@@ -0,0 +1,13 @@
+package com.example.couponsystem.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CouponRequest {
+
+    private Long couponId;
+
+    private Long userId;
+}
